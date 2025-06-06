05879ccc6b1f5d5ba12b0ebefdcd6d3ff6486e09
BirendraRai1 <birendrabit123@gmail.com>
2025-06-06 17:44:17 +0530

    mode changes in cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json({}));
 app.use(cors({
-  origin: process.env.FRONTEND_URL 
+  origin: "https://telegram-weather-bot-aa3a.onrender.com/" 
 }));
 const PORT = process.env.PORT || 3000;
 
