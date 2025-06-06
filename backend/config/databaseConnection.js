8031fb1213f307b4e058ffdea971ef774e35026c
BirendraRai1 <birendrabit123@gmail.com>
2025-06-06 16:12:22 +0530

    modified the database connection

--- backend/config/databaseConnection.js
+++ backend/config/databaseConnection.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
-const connectDB = async () => {
+const { DB_PASSWORD, DB_USER,DB_NAME} = process.env;
+const dbURL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.dk5ax2e.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+const connectDB = async()=>{
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(dbURL);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
     process.exit(1);
   }
-};
+}  
+
+
 
 module.exports = connectDB;
--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -13,7 +13,7 @@ const getUsers = async (req, res) => {
 const blockUnblockUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
-    let blocked  = req.body.blocked;
+    let blocked = req.body.blocked;
     if (!user) return res.status(404).json({ error: "User not found" });
 
     user.isBlocked = blocked;
@@ -30,7 +30,6 @@ const blockUnblockUser = async (req, res) => {
   }
 };
 
-
 const getSettings = async (req, res) => {
   try {
     const settings = await Setting.findOne({});
@@ -42,19 +41,27 @@ const getSettings = async (req, res) => {
 
 const updateSetting = async (req, res) => {
   try {
-    const { apiKey,key, value } = req.body;
-    console.log("apikey,key, value ",apiKey,key, value)
+    const { apiKey, key, value } = req.body;
+    console.log("apikey,key, value ", apiKey, key, value);
 
     if (!key || !value) {
       return res.status(400).json({ error: "Key and value required" });
     }
+    let setting;
+    const findSetting = await Setting.find({ key: apiKey });
+    console.log("findSetting is",findSetting)
+    if (!findSetting){
+      setting = await Setting.create({ key, value });
+      console.log("setting inside if ",setting)
+    }else {
+      setting = await Setting.findOneAndUpdate(
+        { key: apiKey },
+        { value },
+        { new: true, upsert: true }
+      );
+    }
 
-    const setting = await Setting.findOneAndUpdate(
-      { key:apiKey },
-      { key,value },
-      {new:true}
-    );
-    console.log("setting is ",setting)
+    console.log("setting is ", setting);
     res.json(setting);
   } catch (error) {
     console.log("error is", error);
